1a0350a732528a39fbd46b485ab605b9777d5888
stayal2 <sakshitayal712@gmail.com>
2026-02-10 00:46:03 -0500

    update ignore files for .pem and cors for https

--- langchain-agent-chat-system/agent-backend/app/api/middleware.py
+++ langchain-agent-chat-system/agent-backend/app/api/middleware.py
@@ -7,7 +7,8 @@ def setup_cors(app: FastAPI):
     origins = [
         "http://localhost",
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://chat.hug.local"
     ]
 
     app.add_middleware(
