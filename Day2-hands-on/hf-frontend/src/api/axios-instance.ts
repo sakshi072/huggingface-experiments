1fbc0ca4f191e0bca2fb18edf0d662e98a5367b8
stayal2 <sakshitayal712@gmail.com>
2026-02-10 00:44:40 -0500

    routing to https backend

--- Day2-hands-on/hf-frontend/src/api/axios-instance.ts
+++ Day2-hands-on/hf-frontend/src/api/axios-instance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 import type {AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 import { TOKEN_STORAGE_KEYS } from '../config/auth0-config';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://chat.hug.local';
 
 const apiClient: AxiosInstance = axios.create({
     baseURL: API_BASE_URL,
