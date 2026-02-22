8c01fbde73920138b5b7a697b77508fa1f2676cf
stayal2 <sakshitayal712@gmail.com>
2026-02-22 17:59:35 -0500

    exporting mcp client methods

--- langchain-agent-chat-system/agent-backend/app/clients/__init__.py
+++ langchain-agent-chat-system/agent-backend/app/clients/__init__.py
@@ -1,7 +1,8 @@
 from app.clients.rag_client import get_rag_client
-from app.clients.mcp_client import get_mcp_tools
+from app.clients.mcp_client import get_mcp_client, get_mcp_tools
 
 __all__ = [
     "get_rag_client",
-    "get_mcp_tools"
+    "get_mcp_tools",
+    "get_mcp_client"
 ]
